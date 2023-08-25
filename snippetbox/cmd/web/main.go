4d12b90cf18332da883b5d948a97c08e95ae1d82
Manapon Sanguanklin <manapon7330@gmail.com>
2023-08-25 23:06:59 +0700

    Finish Chapter 4.7

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -1,26 +1,42 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/ManaponS/Let-s_GO_Go_Learn/snippetbox/internal/models"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
 	addr := flag.String("addr", "127.0.0.1:4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:7979@/snippetbox?parseTime=true", "MySQL data source name")
+
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	srv := &http.Server{
@@ -29,7 +45,18 @@ func main() {
 		Handler:  app.routes(),
 	}
 
-	infoLog.Println("http://localhost:4000/")
-	err := srv.ListenAndServe()
+	infoLog.Println("http://localhost:4000/", *addr)
+	err = srv.ListenAndServe()
 	errorLog.Fatalln(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
--- snippetbox/go.mod
+++ snippetbox/go.mod
@@ -1,3 +1,5 @@
 module github.com/ManaponS/Let-s_GO_Go_Learn/snippetbox
 
 go 1.20
+
+require github.com/go-sql-driver/mysql v1.7.1 // indirect
