c8d681a8178066f9c69adcd9b71f5a351c5c19b4
Manapon Sanguanklin <manapon7330@gmail.com>
2023-08-25 14:06:51 +0700

    Finish chapter 2

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -7,11 +7,15 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("../../ui/static"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Println("http://localhost:4000/")
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe("127.0.0.1:4000", mux)
 	log.Fatalln(err)
 }
