1311a34a12f2ec73eaaeee9e80f3709efe939bfc
Manapon Sanguanklin <manapon7330@gmail.com>
2023-08-30 11:56:55 +0700

    Finish Chapter 10

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"database/sql"
 	"flag"
 	"html/template"
@@ -51,6 +52,7 @@ func main() {
 	sessionManager := scs.New()
 	sessionManager.Store = mysqlstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
+	sessionManager.Cookie.Secure = true
 
 	app := &application{
 		errorLog:       errorLog,
@@ -61,14 +63,22 @@ func main() {
 		sessionManager: sessionManager,
 	}
 
-	srv := &http.Server{
-		Addr:     *addr,
-		ErrorLog: errorLog,
-		Handler:  app.routes(),
+	tlsConfig := &tls.Config{
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
 	}
 
-	infoLog.Println("http://localhost:4000/", *addr)
-	err = srv.ListenAndServe()
+	srv := &http.Server{
+		Addr:         *addr,
+		ErrorLog:     errorLog,
+		Handler:      app.routes(),
+		TLSConfig:    tlsConfig,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	infoLog.Println("https://localhost:4000/", *addr)
+	err = srv.ListenAndServeTLS("../../tls/cert.pem", "../../tls/key.pem")
 	errorLog.Fatalln(err)
 }
 
